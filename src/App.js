ce813a83526342d4d2bd4754f149cb25fe1b1bb1
XuanDuy2309 <119564395+XuanDuy2309@users.noreply.github.com>
2024-08-22 00:15:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="from-login">
+        <h2>
+          Log in
+        </h2>
+        <form action='' className="form-in">
+          <ul className="">
+            <li className="">Email: <input type='email' placeholder='Email' className='email-in' required=""/></li>
+            <li className="">Password: <input type='password' placeholder='Password' required=""/></li>
+          </ul>
+        </form>
+        <div className="btn-submit">
+          <input type="submit" className="btn" placeholder='Log in' />
+        </div>
+      </div>
     </div>
   );
 }
